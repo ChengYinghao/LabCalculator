9712fc4f8a3d53bebe692f1aa47cd65338f2f418
yinghao <1131637423@qq>
2017-03-08 21:10:20 +0800

    没什么

--- app/src/main/java/cyh/labCalculator/MainActivity.kt
+++ app/src/main/java/cyh/labCalculator/MainActivity.kt
@@ -26,8 +26,8 @@ class MainActivity : Activity(), View.OnClickListener {
 	private val buttonDL0 by lazy { findViewById(R.id.b_Dl0) as Button }
 	private val buttonDx by lazy { findViewById(R.id.b_Dx) as Button }
 	private val buttonDy by lazy { findViewById(R.id.b_Dy) as Button }
-    private val buttonpoint by lazy{findViewById(R.id.point)as Button}
-	
+	private val buttonpoint by lazy { findViewById(R.id.point) as Button }
+
 	private var str = ""
 	private var f: Double = 0.0
 	private var p: Double = 0.0
@@ -42,14 +42,14 @@ class MainActivity : Activity(), View.OnClickListener {
 	private var Um0: Double = 0.0
 	private var Ul0: Double = 0.0
 	private var Ub: Double = 0.0
-	
+
 	public override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 		output.setText("请输入f,m0,l0,Dm,Dl0,Dx,Dy的值")
-		
+
 		input.setText(str)
-		
+
 		button0.setOnClickListener(this)
 		button1.setOnClickListener(this)
 		button2.setOnClickListener(this)
@@ -67,74 +67,86 @@ class MainActivity : Activity(), View.OnClickListener {
 		buttonDL0.setOnClickListener(this)
 		buttonDx.setOnClickListener(this)
 		buttonDy.setOnClickListener(this)
-        buttonpoint.setOnClickListener(this)
+		buttonpoint.setOnClickListener(this)
+
 	}
-	
-	override fun onClick(view: View) {
-		when (view.id) {
-			R.id.b_zero -> num(0)
-			R.id.b_one -> num(1)
-			R.id.b_two -> num(2)
-			R.id.b_three -> num(3)
-			R.id.b_four -> num(4)
-			R.id.b_five -> num(5)
-			R.id.b_six -> num(6)
-			R.id.b_seven -> num(7)
-			R.id.b_eight -> num(8)
-			R.id.b_nine -> num(9)
-			R.id.point -> addPoint()
-			R.id.b_f -> {
-				f = java.lang.Double.parseDouble(str)
-				str = ""
+		override fun onClick(view: View) {
+
+			when (view.id) {
+				R.id.b_zero -> num(0)
+				R.id.b_one -> num(1)
+				R.id.b_two -> num(2)
+				R.id.b_three -> num(3)
+				R.id.b_four -> num(4)
+				R.id.b_five -> num(5)
+				R.id.b_six -> num(6)
+				R.id.b_seven -> num(7)
+				R.id.b_eight -> num(8)
+				R.id.b_nine -> num(9)
+				R.id.point -> addPoint()
+				R.id.b_f -> {
+					f = java.lang.Double.parseDouble(str)
+					str = ""
+					check()
+				}
+				R.id.b_m0 -> {
+					m0 = java.lang.Double.parseDouble(str)
+					str = ""
+					check()
+				}
+				R.id.b_l0 -> {
+					l0 = java.lang.Double.parseDouble(str)
+					str = ""
+					check()
+				}
+				R.id.b_Dm -> {
+					Dm = java.lang.Double.parseDouble(str)
+					str = ""
+					check()
+				}
+				R.id.b_Dl0 -> {
+					Dl0 = java.lang.Double.parseDouble(str)
+					str = ""
+					check()
+				}
+				R.id.b_Dx -> {
+					Dx = java.lang.Double.parseDouble(str)
+					str = ""
+					check()
+				}
+				R.id.b_Dy -> {
+					Dy = java.lang.Double.parseDouble(str)
+					str = ""
+					check()
+				}
 			}
-			R.id.b_m0 -> {
-				m0 = java.lang.Double.parseDouble(str)
-				str = ""
-			}
-			R.id.b_l0 -> {
-				l0 = java.lang.Double.parseDouble(str)
-				str = ""
-			}
-			R.id.b_Dm -> {
-				Dm = java.lang.Double.parseDouble(str)
-				str = ""
-			}
-			R.id.b_Dl0 -> {
-				Dl0 = java.lang.Double.parseDouble(str)
-				str = ""
-			}
-			R.id.b_Dx -> {
-				Dx = java.lang.Double.parseDouble(str)
-				str = ""
-			}
-			R.id.b_Dy -> {
-				Dy = java.lang.Double.parseDouble(str)
-				str = ""
-			}
+
 		}
-		if (f * m0 * l0 * Dm * Dl0 * Dx * Dy != 0.0) {
-			uncertaintyCount()
-			output.setText("Uf:$Uf Up:$Up Ub:$Ub ")
+
+
+		fun uncertaintyCount() {
+			Ub = Math.sqrt(Math.pow(Dx, 2.0) / 12 + Math.pow(Dy, 2.0) / 3)
+			p = m0 / l0
+			Um0 = Dm / Math.sqrt(3.0)
+			Ul0 = Dl0 / Math.sqrt(3.0)
+			Up = Math.sqrt(Math.pow(Um0 / m0, 2.0) + Math.pow(Ul0 / l0, 2.0)) * p
+			Uf = Math.sqrt(Math.pow(Ub, 2.0) + Math.pow(Up / p, 2.0) / 4) * f
 		}
-	}
-	
-	
-	fun uncertaintyCount() {
-		Ub = Math.sqrt(Math.pow(Dx, 2.0) / 12 + Math.pow(Dy, 2.0) / 3)
-		p = m0 / l0
-		Um0 = Dm / Math.sqrt(3.0)
-		Ul0 = Dl0 / Math.sqrt(3.0)
-		Up = Math.sqrt(Math.pow(Um0 / m0, 2.0) + Math.pow(Ul0 / l0, 2.0)) * p
-		Uf = Math.sqrt(Math.pow(Ub, 2.0) + Math.pow(Up / p, 2.0) / 4) * f
-	}
-	
-	fun num(n: Int) {
-		str += n.toString()
-		input.setText(str)
-	}
-	
-	fun addPoint() {
-		str += "."
-		input.setText(str)
+
+		fun num(n: Int) {
+			str += n.toString()
+			input.setText(str)
+		}
+
+		fun addPoint() {
+			str += "."
+			input.setText(str)
+		}
+	fun check(){
+		if(f * m0 * l0 * Dx * Dy * Dl0 * Dm != 0.0){
+			uncertaintyCount();
+			output.setText("Uf"+Uf+" "+"Ub"+Ub+" "+"Up"+Up)
+		}
 	}
 }
+
