fd465074b95da14c27a7b6e34c6b8a54e30c6e06
keli <zhengkeli2009@126.com>
2017-04-01 22:02:29 +0800

    写了一个 test

--- core/src/test/java/cyh/labCalculator/coreTest/CoreTest.kt
+++ core/src/test/java/cyh/labCalculator/coreTest/CoreTest.kt
@@ -1,5 +1,24 @@
 package cyh.labCalculator.coreTest
 
-/**
- * Created by zheng on 2017/4/1.
- */
+import cyh.labCalculator.core.MeasuredQuantity
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.BlockJUnit4ClassRunner
+
+@RunWith(BlockJUnit4ClassRunner::class)
+class CoreTest{
+	
+	@Test
+	fun testMeasuredQuantity(){
+		val measuredValues = arrayListOf(5.5, 4.3, 5.3, 5.2)
+		val measuredQuantity = MeasuredQuantity(measuredValues, 0.1/Math.sqrt(3.0))
+		
+		
+		println("average:${measuredQuantity.value}")
+		println("count:${measuredQuantity.valueCount}")
+		println("uncertaintyA:${measuredQuantity.uncertaintyA}")
+		println("uncertaintyB:${measuredQuantity.uncertaintyB}")
+		println("uncertainty:${measuredQuantity.uncertainty}")
+		
+	}
+}
