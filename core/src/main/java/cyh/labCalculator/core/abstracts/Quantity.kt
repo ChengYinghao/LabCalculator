ec1632179f5005cca92d4465fcb3a2db8dc0fba8
keli <zhengkeli2009@126.com>
2017-04-04 12:19:06 +0800

    加了一点计算图的接口内容（实体部分写起来还蛮麻烦的）

--- core/src/main/java/cyh/labCalculator/core/abstracts/Quantity.kt
+++ core/src/main/java/cyh/labCalculator/core/abstracts/Quantity.kt
@@ -3,3 +3,12 @@ package cyh.labCalculator.core.abstracts
 interface Quantity : LabObject {
 	val value:Double
 }
+class ConstantQuantity(override val value:Double):Quantity
+
+
+interface RelatedQuantity:Quantity{
+	val arguments:MutableList<Quantity>
+}
+interface DerivableQuantity:RelatedQuantity{
+	fun derivative(target: Quantity): Quantity
+}
